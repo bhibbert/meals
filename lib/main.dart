da8b4119d40882cc0679b3b46c82fb8c38cbaf94
Brian Hibbert <BHibbert@ictsi.com>
2023-11-10 13:29:34 +0800

    Added ability to click through to the Meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
-// import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
+import 'package:meals/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,8 +22,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: theme,
-        // home: const CategoriesScreen(),
-        home: const MealsScreen(title: 'Some category...', meals: dummyMeals));
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
   }
 }
