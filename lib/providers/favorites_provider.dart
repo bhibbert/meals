e6f3e1aed81e3dd602c2ad8fc3495d5034d8ccee
Brian Hibbert <brian@thehibberts.net>
2023-11-15 19:21:03 +0800

    Added providers shifting state to providers.

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -4,15 +4,13 @@ import 'package:meals/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  bool toggleMealFavoriteStatus(Meal meal) {
+  void toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
-      return false;
     } else {
       state = [...state, meal];
-      return true;
     }
   }
 }
